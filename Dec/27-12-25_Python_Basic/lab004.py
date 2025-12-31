d484dcd4e7989782c2cc69b6e571b0f674d50ae3
Jibi Thomsd <jibithomas014@gmail.com>
2025-12-31 13:28:11 +0530

    input fn

---
+++ Dec/27-12-25_Python_Basic/lab004.py
@@ -0,0 +1,9 @@
+
+name = input("enter your name")
+print("your name is", name)
+
+name2 = input("enter your name2")
+print("your name is", name2)
+
+########################################
+
---
+++ Dec/27-12-25_Python_Basic/lab005.py
@@ -0,0 +1,16 @@
+
+
+
+num1 = int(input("enter the number1"))
+num2 = int(input("enter your number2"))
+
+sum = num1+num2
+sub = num2-num1
+mul = num1*num2
+div = num1/num2
+
+print("sum is", sum)
+print("sub is", sub)
+print("mul is", mul)
+print("div is", div)
+

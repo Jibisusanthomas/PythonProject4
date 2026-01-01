7e6f6525f8f362dc87c3d3702df7f1a9fe5b16d8
Jibi Thomsd <jibithomas014@gmail.com>
2026-01-01 22:30:43 +0530

    Literals

---
+++ Dec/27-12-25_Python_Basic/lab006.py
@@ -0,0 +1,22 @@
+
+name = "Donna"
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(len(name))
+
+print(name + str(1))
+
+name2 = "Harvey"
+print(name +' ' + name2)
+print("Donna\nHarvey")
+print("Donna\bHarvey")
+print("Donna\tHarvey")
+
+# r before the string makes it a raw string so backslashes are not treated as escape characters
+dir = r"C:\Pramod\t.txt"
+print(dir)
+dir2 = r"C:\Pramod\n.txt"
+print(dir2)
+dir3 = r"C:\Pramod\b.txt"
+print(dir3)
